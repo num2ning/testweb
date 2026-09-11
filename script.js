95b24b9753d12db6ef4d8ad1ce607b9b0fc3909f
num2ning <118881528+num2ning@users.noreply.github.com>
2026-09-11 14:21:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ function startScanner() {
 
             html5QrCode.start(
                 { facingMode: "environment" },
-                { fps: 25, qrbox: { width: 250, height: 250 } },
+                { fps: 30, qrbox: { width: 250, height: 250 } },
                 onScanSuccess
             ).then(() => {
                 document.getElementById('overlay').style.display = 'flex';
